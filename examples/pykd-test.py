dd50f3a6b2d009ad8fe8b5b6558b1ac24de79c7c
Alexander Polev <buridan@inbox.ru>
2025-05-16 02:05:04 +0500

    example add more cmds

--- examples/pykd-test.py
+++ examples/pykd-test.py
@@ -3,4 +3,28 @@
 
 import pykd
 
-print(pykd.dbgCommand("kp"))
+def my_function():
+    print('BP trig')
+
+#print(pykd.dbgCommand("kp"))
+
+#pykd.callFunctionRaw(("DbgPrint", "message"))
+result = pykd.getSystemVersion()
+print(result)
+
+nt_module = pykd.module("nt").begin()
+print(nt_module)
+
+nt_DbgPrint = pykd.getOffset('nt!DbgPrint')
+print(nt_DbgPrint)
+
+bp = pykd.setBp(nt_DbgPrint, my_function)
+print(bp)
+#help(bp)
+
+bp_id = bp.getId()
+print(bp_id)
+
+pykd.dprintln("<b><u>The following command reloads all symbols</b></u>", True)
+pykd.dprintln("<link cmd=\".reload /f\">reload</link>", True)
+
